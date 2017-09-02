9934920cb2c83a34a69ce5b6e6b9cabcdabc5764
Mynik3d <sanamah550@gmail.com>
2017-09-03 01:12:33 +0300

    slider

---
+++ js/slick-ml.js
@@ -0,0 +1,8 @@
+$('.most-l').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: '<div class="prev-ml"></div>',
+nextArrow: '<div class="next-ml"></div>'
+});
