8621f2e267d424c8ca547c601802f21250083d43
Mynik3d <sanamah550@gmail.com>
2017-09-07 00:56:53 +0300

    about block

---
+++ js/slick.js
@@ -0,0 +1,18 @@
+$('.sl').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  // autoplay: true,
+  autoplaySpeed: 5000,
+  initialSlide: 1,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  arrows: true,
+  cssEase: 'linear',
+  prevArrow: '<div class="prev"></div>',
+nextArrow: '<div class="next"></div>'
+
+
+});
