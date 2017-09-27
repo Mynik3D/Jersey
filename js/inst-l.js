1d38cdbc67d2c31f2e653dd55a54ef4311339767
Mynik3d <sanamah550@gmail.com>
2017-09-27 22:23:36 +0300

    feature block

---
+++ js/inst-l.js
@@ -0,0 +1,7 @@
+// $('.inst').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplaySpeed: 2000,
+//   prevArrow: '<div class="prev-ml"></div>',
+// nextArrow: '<div class="next-ml"></div>'
+// });
