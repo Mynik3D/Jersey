f2f592c358eef0468b7ccd5df050c25310868c96
Mynik3d <sanamah550@gmail.com>
2017-09-28 21:22:53 +0300

    the end

--- js/valuesrch.js
+++ js/valuesrch.js
@@ -12,3 +12,18 @@ function resetSearchInput(input) {
         input.value = searchText;
     }
 }
+var searchText = "Sign up for style news";
+function clearSearchInput(input) {
+    if (input.value === searchText) {
+        input.value = "";
+       
+
+    }
+
+}
+function resetSearchInput(input) {
+    if (input.value === "") {
+        input.value = searchText;
+    }
+}
+
